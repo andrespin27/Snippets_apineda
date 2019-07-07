18d903bb170df1cd698d6a34a950f8f035612d1a
apineda <andrespin27gmail.com>
2019-07-07 18:53:43 -0500

    unpivot

--- sql/snippets_SQL.sql
+++ sql/snippets_SQL.sql
@@ -40,3 +40,34 @@ INSERT INTO NombreNuevaTabla
 SELECT 
 Campos....
 FROM TablaOrigen
+
+
+/********************Convertir fecha como ID (20190101)**************/
+
+
+(SELECT CONVERT(VARCHAR(10), fecha, 112))
+
+
+
+
+
+/********************UNPIVOT**************/
+Select 
+Campo1,/*Campos que se mantienen como estaticos*/
+CAmpoN../*Campos que se mantienen como estaticos*/
+Fecha/*Nombre de Campo Donde Almacenaremos las Fecha 'Este Campo No existe en la Tabla'*/
+Valor /*Nombre de Campo Donde Almacenaremos los Valores 'Este Campo No existe en la Tabla'*/
+Into Bi_Compe_Conso_Gral_Mdellin_UNP /*Tabla a donde llevaremos los datos*/
+from Bi_Compe_Conso_Gral_Mdellin
+UNPIVOT
+(
+Valor /*Nombre del campo donde se almacenaran los valores 'Lo creamos en el Select'*/
+FOR
+Fecha IN /*Este es el campo que se creo para almacenar las fecha*/
+(
+/*Nombre de los encabezados de Columnas que vamos a Trasponer*/
+[02 01 2019],
+[08 01 2019],
+[08 01 2019]...
+)
+)AS p
